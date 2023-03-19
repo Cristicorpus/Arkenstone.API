5d9652b0b91c02c873eabed99f4d524603644147
vincent perez <perez.vincent21@gmail.com>
2023-03-18 22:13:34 -0400

    Update InventoryModels.cs

--- Arkenstone.API/Models/InventoryModels.cs
+++ Arkenstone.API/Models/InventoryModels.cs
@@ -1,23 +1,71 @@
 ﻿using Arkenstone.Entities.DbSet;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Arkenstone.API.Models
 {
+
     public class InventoryModel
     {
-        public InventoryModel()
-        { }
-        public int Quantity { get; set; }
+        public long quantity { get; set; }
+        public Item ItemModel { get; set; }
 
-        public virtual Item Item { get; set; }
-
-        public virtual SubLocation SubLocation { get; set; }
-
-
-        public InventoryModel(Inventory inventory)
+        public InventoryModel(Item item, long quantity)
         {
-            this.Quantity = inventory.Quantity;
-            this.Item = inventory.Item;
-            this.SubLocation = inventory.SubLocation;
+            this.quantity = quantity;
+            this.ItemModel = item;
+        }
+
+    }
+    public class InventorySubLocationModel
+    {
+        public SubLocation subLocation { get; set; }
+
+        public List<InventoryModel> Assets { get; set; }
+
+        public InventorySubLocationModel(SubLocation target)
+        {
+            this.subLocation = target;
+            this.Assets = new List<InventoryModel>(); ;
+        }
+        public InventorySubLocationModel(SubLocation target, List<Entities.DbSet.Inventory> inventorys)
+        {
+            this.subLocation = target;
+            this.Assets = new List<InventoryModel>();
+            foreach (var asset in inventorys)
+            {
+                var temp = Assets.FirstOrDefault(x => x.ItemModel.Id == asset.Item.Id);
+                if (temp == null)
+                    Assets.Add(new InventoryModel(asset.Item, asset.Quantity));
+                else
+                    temp.quantity += asset.Quantity;
+            }
         }
     }
+    public class InventoryStationModel
+    {
+        public LocationModel structure { get; set; }
+
+        public List<InventoryModel> Assets { get; set; }
+
+        public InventoryStationModel(Location target)
+        {
+            this.structure = new LocationModel(target);
+            this.Assets = new List<InventoryModel>(); ;
+        }
+        public InventoryStationModel(Location target, List<Entities.DbSet.Inventory> inventorys)
+        {
+            this.structure = new LocationModel(target);
+            this.Assets = new List<InventoryModel>();
+            foreach (var asset in inventorys)
+            {
+                var temp = Assets.FirstOrDefault(x => x.ItemModel.Id == asset.Item.Id);
+                if (temp == null)
+                    Assets.Add(new InventoryModel(asset.Item, asset.Quantity));
+                else
+                    temp.quantity += asset.Quantity;
+            }
+        }
+    }
+
 }
