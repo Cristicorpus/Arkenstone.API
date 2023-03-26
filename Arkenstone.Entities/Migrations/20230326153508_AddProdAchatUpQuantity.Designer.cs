e7ccfd93504d9d224752fda3082517e9b857c843
vincent perez <perez.vincent21@gmail.com>
2023-03-26 11:35:22 -0400

    update quantity

--- Arkenstone.Entities/Migrations/20230326153508_AddProdAchatUpQuantity.Designer.cs
+++ Arkenstone.Entities/Migrations/20230326153508_AddProdAchatUpQuantity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Arkenstone.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Arkenstone.Entities.Migrations
 {
     [DbContext(typeof(ArkenstoneContext))]
-    partial class ArkenstoneContextModelSnapshot : ModelSnapshot
+    [Migration("20230326153508_AddProdAchatUpQuantity")]
+    partial class AddProdAchatUpQuantity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Arkenstone.Entities/Migrations/20230326153508_AddProdAchatUpQuantity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Arkenstone.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProdAchatUpQuantity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Quantity",
+                table: "ProdAchats",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantity",
+                table: "ProdAchats",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
